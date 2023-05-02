837d95e7ed9e84812e895a1eac5228a7a7ae9b12
Débora Linhares <deboralinharespinheiro@gmail.com>
2023-05-02 12:22:01 -0300

    Débora

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const DigiteUmNumero = prompt("Digite um número");
+const Dobro = DigiteUmNumero * 2;
+const Resultado = alert("O dobro do seu número é " + Dobro);
+
+
+const ValorDaConta = prompt("Valor da Conta");
+//const TaxaDoGarçom = ValorDaConta * 0.10;
+const ValorTotal =  ValorDaConta * 0.10 + ValorDaConta;
+const ValorFinal = alert("O Valor a ser pago é: "+ ValorTotal);
+
+const ContaDaPizza = prompt("Valor da conta");
+const QuantidadeDePessoas = prompt("Quantas pessoas vão dividir");
+const Total = ContaDaPizza / QuantidadeDePessoas;
+const ValorPorPessoa = alert("Valor por pessoa: "+ Total);
+
+const nome = prompt("Qual o seu nome completo?");
+const idade= prompt("Qual a sua idade?");
+const Endereço = prompt("Qual o seu endereço?"); 
+const cor = prompt("Qual a sua cor favorita?"); 
+const animal = prompt("Você tem algum animal de estimação?");
+
+
+
